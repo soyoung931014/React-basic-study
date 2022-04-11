f5cd8033bf64601639c824e50044b80b87349e99
park_so_young <soyoung931014@gmail.com>
2022-04-11 12:16:57 +0900

    ADD: 6-8 React에서 API호출하기:

--- diary/src/App.js
+++ diary/src/App.js
@@ -10,6 +10,28 @@ function App() {
 
   const dataId = useRef(0)
   //일기 배열에 새로운 데이터를 추가시키는
+  const getData = async() => { 
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments")
+    .then(res => res.json()); // json()실행버튼 꼭 넣어줘야함
+
+    const ititialData = res.slice(0,20).map((el) => {
+      return {
+        author: el.email,
+        content: el.body,
+        emotion: Math.floor(Math.random()* 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+
+    setData(ititialData)
+  };
+  
+  useEffect(() => {
+    getData()
+  },[])
+
+
 
  
   const onCreate = (author, content, emotion) => {
