145e92ddfe40fb5c3edd3b4cea5898d964b8dd55
park_so_young <soyoung931014@gmail.com>
2022-07-04 18:49:26 +0900

    ADD: useMemo

--- diary/src/App.js
+++ diary/src/App.js
@@ -9,32 +9,30 @@ import { useState, useRef, useEffect, useMemo } from 'react'
 function App() {
   const [data, setData] = useState([]);
 
-  const dataId = useRef(0)
+  const dataId = useRef(0);
   //일기 배열에 새로운 데이터를 추가시키는
-  const getData = async() => { 
-    const res = await fetch("https://jsonplaceholder.typicode.com/comments")
-    .then(res => res.json()); // json()실행버튼 꼭 넣어줘야함
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json()); // json()실행버튼 꼭 넣어줘야함
 
-    const ititialData = res.slice(0,20).map((el) => {
+    const ititialData = res.slice(0, 20).map((el) => {
       return {
         author: el.email,
         content: el.body,
-        emotion: Math.floor(Math.random()* 5) + 1,
+        emotion: Math.floor(Math.random() * 5) + 1,
         created_date: new Date().getTime(),
-        id: dataId.current++
-      }
+        id: dataId.current++,
+      };
     });
 
-    setData(ititialData)
+    setData(ititialData);
   };
-  
+
   useEffect(() => {
     getData();
-  },[])
- 
+  }, []);
 
-
- 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -42,31 +40,47 @@ function App() {
       content,
       emotion,
       created_date,
-      id: dataId.current
-    }
+      id: dataId.current,
+    };
     dataId.current += 1;
-    setData([newItem, ...data])
-  }
+    setData([newItem, ...data]);
+  };
 
   const onRemove = (id) => {
-    const RemoveList = data.filter(el => el.id !== id)
-    setData(RemoveList)
-  }
+    const RemoveList = data.filter((el) => el.id !== id);
+    setData(RemoveList);
+  };
   const onEdit = (targetId, newContent) => {
     setData(
-      data.map(el => el.id === targetId ? {...el, content: newContent} : el)
-    )
-  }
+      data.map((el) =>
+        el.id === targetId ? { ...el, content: newContent } : el
+      )
+    );
+  };
+
+  //🌹 usMemo사용전: 연산 최적화가 안됨
+  //  const getDiaryAnalysis = () => {
+  //    console.log("일기 분석 시작");
+  //    const goodCount = data.filter((el) => el.emotion >= 3).length;
+  //    const badCount = data.length - goodCount;
+  //    const goodRatio = (goodCount / data.length) * 100;
+  //    return { goodCount, badCount, goodRatio };
+  //  }
+
+  //  const { goodCount, badCount, goodRatio } = getDiaryAnalysis();
+
+  // 🌹 usMemo사용 후, 데이터의 길이가 변할때만 렌더링됨(최적화됨)
   const getDiaryAnalysis = useMemo(() => {
-    console.log('일기 분석 시작');
-    const goodCount = data.filter(el => el.emotion >=3).length;
+    console.log("일기 분석 시작");
+    const goodCount = data.filter((el) => el.emotion >= 3).length;
     const badCount = data.length - goodCount;
     const goodRatio = (goodCount / data.length) * 100;
-    return {goodCount, badCount, goodRatio}
-  }, [data.length])
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
 
-  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
-  // getDiaryAnalysis()이렇게 하면 오류뜸. 왜냐면 변수 getDiartAnalysis는 return값이 있는 '값'이지 '함수'가 아니기 때문
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+  // getDiaryAnalysis()이렇게 하면 오류뜸. 왜냐면 useMemo를 사용한 함수는 값을 리턴하기 때문.
+  // 더이성 변수 getDiartAnalysis는 useMemo를 사용하면 return값이 있는 '값'이지 '함수'가 아니기 때문
   // useMemo는 값을 받는다.
 
   return (
@@ -79,7 +93,6 @@ function App() {
       <div>기분 나쁜 일기 개수 : {badCount}</div>
       <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} diaryList={data} onRemove={onRemove} />
-
     </div>
   );
 }
